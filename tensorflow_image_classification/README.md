38c943956de7e3f500d91696878dfe2c6eb3da01
Yu Sun <ysun@Yus-MacBook-Air-2.local>
2017-09-25 10:33:58 -0700

    tensorflow image classifcation GET example

---
+++ tensorflow_image_classification/README.md
@@ -0,0 +1,3 @@
+Google Sample Code for Image Classification using Tensorflow.
+GET request asks TF to download an image from URL
+Then return a JSON object with the probability of seeing items such as teddy bear and more
---
+++ tensorflow_image_classification/tf_image_recog_resultJSON.js
@@ -0,0 +1,10 @@
+{
+    "image_url": "https://snapshot_url",
+    "results": {
+        "teddy, teddy bear": 84.68%
+        "seat belt, seatbelt": 0.85%
+        "toyshop": 0.37%
+        "ballpoint, ballpoint pen, ballpen, Biro": 0.25%
+        "hamper": 0.18%
+    }
+}
